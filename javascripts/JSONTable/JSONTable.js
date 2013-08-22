9174bf0dfbda6691598a79708d4e0419578f8e7f
Jon <jon@6eDesign.net>
2013-08-22 13:27:28 -0600

    updated jsontable.js on gh-pages.  will do more testing and push to master.

--- javascripts/JSONTable/JSONTable.js
+++ javascripts/JSONTable/JSONTable.js
@@ -3,17 +3,25 @@
 	var $, createElem, creator, 
 	__indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
+	var defaultopts = { 
+		// default opts go here:
+		show: [], 
+		numCols: 6,
+		trim: [],
+		keyMap: {}, 
+		filterMap: {}, 
+		breakpoints: { phone: 480, tablet: 1024, desktop: 10000 }, 
+		searchId: '', 
+		paginationId: '', 
+		perPage: 10, 
+		paginate: false, 
+		makeFootable: true, 
+		deepScan: false
+	}
+	
 	window.create = function(obj) {
 		return creator(obj);
 	};
-	
-	var extend = function() { 
-	    for(var i=1; i<arguments.length; i++)
-	        for(var key in arguments[i])
-	            if(arguments[i].hasOwnProperty(key))
-	                arguments[0][key] = arguments[i][key];
-	    return arguments[0];
-	}
 
 	creator = function(obj) {
 		var contentsObj, elem, innerElem, _i, _len, _ref;
@@ -51,18 +59,12 @@
 		return elem;
 	};
 
-	var defaultopts = { 
-		// default opts go here:
-		show: 'first_6', 
-		trim: [],
-		keyMap: {}, 
-		filterMap: {}, 
-		breakpoints: { phone: 480, tablet: 1024, desktop: 10000 }, 
-		searchId: '', 
-		paginationId: '', 
-		perPage: 10, 
-		paginate: false, 
-		makeFootable: true
+	var extend = function() { 
+	    for(var i=1; i<arguments.length; i++)
+	        for(var key in arguments[i])
+	            if(arguments[i].hasOwnProperty(key))
+	                arguments[0][key] = arguments[i][key];
+	    return arguments[0];
 	}
 
 	var inArray = function(has,arr) { 
@@ -73,149 +75,170 @@
 	}
 
 	window.JSONTable = function(id,json,opts) { 
-		return table(id,json,opts); 
+		if(typeof id != "undefined" && id != "" && typeof json != "undefined") { 
+			opts = (typeof opts == "undefined") ? {} : opts; 
+			return table(id,json,opts); 
+		} else { 
+			console.log("JSONTable: 'id' or 'json' parameters were not supplied or are invalid."); 
+		}
 	}
-	var table = function(id, json, opts) { 
-		var targetdiv, columns, settings, table, thead, tbody, columnsJSON, visiblecols; 
 
-		targetdiv = document.getElementById(id); 
-
-		columns = []; columnsJSON = []; visiblecols = []; 
-
-		settings = (typeof opts == "undefined") ? {} : opts; 
-		// settings = $.extend({}, defaultopts, opts);
-		settings = extend(defaultopts, opts);
-
-		if(typeof json != "undefined") { 
-			for(var key in json[0]) { 
-				if(!inArray(key,settings.trim)) { 
-					columns.push(key);  
-					
-					var dataClass = ""; 
-					var dataHide = ""; 
-
-					if(typeof settings.show == "string") { 
-						if(settings.show.indexOf("first_") > -1) { 
-							var maxShown = parseInt(settings.show.split('first_')[1]); 
-						} else { 
-							maxShown = 3; 
-							console.log("JSONTAble: Unrecognized format for 'show' parameter (must be string in form: 'first_@', where @ is an integer or an array of JSON Keys).");
-						}
-						if(columns.length == 1) { 
-							dataClass = "expand"; 
-						} 
-						if(columns.length > maxShown) { 
-							dataHide = "phone,tablet,desktop"; 
-						} else if(columns.length > 2) { 
-							dataHide = "phone,tablet"; 
-						} else if(columns.length > 1) { 
-							dataHide = "phone"; 
-						}
-					} else { 
-						if(inArray(key,settings.show)) { 
-							visiblecols.push(key); 
-							dataHide = ""; 
-							if(visiblecols.length == 1) { 
-								dataClass = "expand"; 
-							}
-							if(visiblecols.length > 2) { 
-								dataHide = "phone,tablet"; 
-							} else if(visiblecols.length > 1) { 
-								dataHide = "phone"; 
-							}					
-						} else { 
-							dataHide = "phone,tablet,desktop"
-						}
-					}
-					
-					if(typeof settings.keyMap[key] != "undefined") { 
-						key = settings.keyMap[key]; 
-					}
-
-					columnsJSON.push({
-						type: 'th', 
-						attributes: {
-							'data-class': dataClass, 
-							'data-hide': dataHide
-						}, 
-						contains: [ key ] 			
-					})
+	// getColumn's only function is to return an ordered list of column keys
+	var getColumns = function(json, settings) { 
+		var columns = []; 
+		for(var i=0; i < settings.show.length; ++i) { 
+			columns.push(settings.show[i]); 
+		}
+		for(var i=0; i < json.length; ++i) { 
+			for(var key in json[i]) { 
+				if( !inArray(key,settings.trim) && !inArray(key,columns) ) { 
+					columns.push(key); 
 				}
 			}
-			var tableAttrs = { 'class': 'footable' }; 
-			if(settings.searchId != "") { 
-				tableAttrs['data-filter'] = "#" + settings.searchId; 
-			}
-			if(settings.paginationId != "") { 
-				settings.paginate = true; 
-				tableAttrs['data-page-navigation'] = "#" + settings.paginationId; 
-				tableAttrs['data-page-size'] = settings.perPage; 
-			} else { settings.paginate = false; } 
+		}
+		return columns; 
+	}
 
-			table = window.create({
-				type: 'table', 
-				attributes: tableAttrs,
-				contains: [
-					{
-						type: 'thead', 
-						attributes: { }, 
-						contains: [
-							{
-								type: 'tr', 
-								attributes: { }, 
-								contains: columnsJSON
-							}
-						] 
+	var table = function(id,json,opts) { 
+		var targetdiv, columns, settings, table, thead, tbody, columnsJSON, tableAttrs; 
+		
+		columnsJSON = [];  
+		targetdiv = document.getElementById(id); 
+
+		settings = extend(defaultopts,opts)
+
+		// send the whole array only if(deepScan):
+		columns = (settings.deepScan) ? getColumns(json, settings) : getColumns([json[0]], settings); 
+
+		for(var i=0; i < columns.length; ++i) { 
+			var columnObj = {}; 
+			columnObj.type = "th"; 
+			columnObj.attributes = {}; 
+
+			// add expand class to first column: 
+			if(i==0) { columnObj.attributes['class'] = "expand" } 
+			
+			if( settings.show.length == 0 ) { 
+				// limit cols shown in desktop breakpoint by settings.numCols
+				if(i > (settings.numCols-1)) { 
+					columnObj.attributes['data-hide'] = "phone,tablet,desktop"; 
+				} else if(i > 2) { 
+					columnObj.attributes['data-hide'] = "phone,tablet"; 
+				} else if(i > 0) { 
+					columnObj.attributes['data-hide'] = "phone"; 
+				}
+			} else { 
+				// limit cols shown by settings.show array
+				if( inArray(columns[i],settings.show) ) { 
+					// this col needs to be shown at desktop breakpoint: 
+					if(i > 2) { 
+						columnObj.attributes['data-hide'] = "phone,tablet"; 
+					} else if(i > 0) { 
+						columnObj.attributes['data-hide'] = "phone"; 
 					}
-				] 
-			})
+				} else { 
+					// this col is not one of those specified in settings.show
+					columnObj.attributes['data-hide'] = "phone,tablet,desktop"; 
+				}
+			}
+			// replace column name from keyMap if a definition exists
+			columnObj.contains = []; 
+			columnObj.contains.push( (typeof settings.keyMap[columns[i]] != "undefined") ? settings.keyMap[columns[i]] : columns[i] ); 
+			columnsJSON.push(columnObj); 
+		}
 
-			tbody = window.create({
-				type: 'tbody', 
-				attributes: { }, 
+		tableAttrs = { 'class': 'footable' }; 
+		if(settings.searchId != "") { 
+			tableAttrs['data-filter'] = "#" + settings.searchId; 
+		}
+		if(settings.paginationId != "") { 
+			settings.paginate = true; 
+			tableAttrs['data-page-navigation'] = "#" + settings.paginationId; 
+			tableAttrs['data-page-size'] = settings.perPage; 
+		} else { 
+			settings.paginate = false; 
+		}
+
+		/* Build the table: */
+		table = create({
+			type: 'table', 
+			attributes: tableAttrs,
+			contains: [
+				{
+					type: 'thead', 
+					attributes: { }, 
+					contains: [
+						{
+							type: 'tr', 
+							attributes: { }, 
+							contains: columnsJSON
+						}
+					] 
+				}
+			] 
+		}); 
+
+		tbody = create({
+			type: 'tbody', 
+			attributes: {}, 
+			contains: []
+		})
+
+		/* Create the rows: */
+		for(var i=0; i < json.length; ++i) { 
+			var row = create({
+				type: 'tr', 
+				attributes: {}, 
 				contains: []
 			})
-
-			for(var i=0; i < json.length; ++i) { 
-				var row = window.create({
-					type: 'tr', 
-					attributes: { }
-				}); 
-				for(var j=0; j < columns.length; ++j) { 
+			for(var j=0; j < columns.length; ++j) { 
+				var className = (j==0) ? "expand" : ""; 
+				if(typeof json[i][columns[j]] == "undefined" || json[i][columns[j]] == null) { 
 					if(json[i][columns[j]] == null) { 
-						json[i][columns[j]] = ""; 
+						var data = "null"; 
+					} else { 
+						var data = ""; 
 					}
-					
+				} else { 
 					var data = json[i][columns[j]]; 
-
-					// check if a filter is defined for this json key: 
+					// check if a filter exists for this key: 
 					if(typeof settings.filterMap[columns[j]] != "undefined") { 
 						// make sure it's a function: 
 						if(typeof window[settings.filterMap[columns[j]]] == "function") { 
 							// call that function: 
 							data = window[settings.filterMap[columns[j]]](json[i][columns[j]]); 
 						}
+					} else if(typeof data == "object") {
+						// This item is an array without a filterMap function, concatenate values:
+						var txt = ""; 
+						for(var k=0; k < data.length; ++k) { 
+							txt += data[k];
+						}
+						data = txt; 
 					}
-					var td = window.create({
-						type: 'td', 
-						attributes: {}, 
-						contains: [ data ]
-					})
-					row.appendChild(td); 
-				} 
-				tbody.appendChild(row); 
-			}
+				}
 
-			table.appendChild(tbody); 
-			targetdiv.appendChild(table); 
+				var td = create({
+					type: 'td', 
+					attributes: { 'class': className }, 
+					contains: [data]
+				});
 
-			if(settings.makeFootable) { 
-				$ = jQuery;
-				$(table).footable({breakpoints: settings.breakpoints, paginate: settings.paginate });
+				row.appendChild(td); 
 			}
-			return table; 
-		} else { 
-			console.log("JSONTable: JSON is undefined"); 
-		}	
+			tbody.appendChild(row); 
+		}
+
+		// join the table, thead, and tbody: 
+		table.appendChild(tbody); 
+
+		targetdiv.appendChild(table); 
+
+		if(settings.makeFootable) { 
+			$ = jQuery;
+			$(table).footable({breakpoints: settings.breakpoints, paginate: settings.paginate });
+		}
+
+		return table; 
 	}
 }).call(this);
